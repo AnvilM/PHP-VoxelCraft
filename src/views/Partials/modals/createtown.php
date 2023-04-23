ec0083889185bd1defbe1c51c8fea5c5dfd91a07
AnvilM <211anvil112@gmail.com>
2023-04-23 18:46:50 +0300

    TOWN CREATE hotfix 🔥

--- src/views/Partials/modals/createtown.php
+++ src/views/Partials/modals/createtown.php
@@ -47,7 +47,7 @@
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Загрузить баннер</label>
                     <input
                         class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-tavern-grid-br dark:border-gray-600 dark:placeholder-gray-400"
-                        aria-describedby="file_input_help" id="file_input" type="file">
+                        aria-describedby="file_input_help" id="file_input" type="file" name="banner">
                     <p class="text-sm text-gray-500 dark:text-gray-300" id="file_input_help">PNG, JPG (MAX. 20x40px).</p>
 
                     <div>
